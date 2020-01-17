f15a9fcd16bc0b85d552f73b8789c60b2ffe4bbd
Andreas Roehler <andreas.roehler@online.de>
2020-01-17 18:05:46 +0100

    Minor fixes

--- thingatpt-utils-core.el
+++ thingatpt-utils-core.el
@@ -2291,22 +2291,26 @@ Otherwise assume being behind an opening delimiter or at a closing "
        (forward-sexp)(point)))
 
 ;; Sh-struct
-(put 'shstruct 'beginning-op-at
-     'sh-beginning-of-form)
+(eval-after-load 'sh-werk
+  (put 'shstruct 'beginning-op-at
+       'sh-beginning-of-form))
 
-(put 'shstruct 'end-op-at
-     (lambda ()
-       (when (looking-at ar-beginning-shstruct-atpt)
-	 (sh-end-of-form)
-	 (forward-char 1)(point))))
+(eval-after-load 'sh-werk
+  (put 'shstruct 'end-op-at
+       (lambda ()
+	 (when (looking-at ar-beginning-shstruct-atpt)
+	   (sh-end-of-form)
+	   (forward-char 1) (point)))))
 
-(put 'shstruct 'forward-op-at
-     (lambda ()
-       (re-search-forward ar-beginning-shstruct-atpt nil 'move 1)))
+(eval-after-load 'sh-werk
+  (put 'shstruct 'forward-op-at
+       (lambda ()
+	 (re-search-forward ar-beginning-shstruct-atpt nil 'move 1))))
 
-(put 'shstruct 'backward-op-at
-     (lambda ()
-       (re-search-backward ar-end-shstruct-atpt nil 'move 1)))
+(eval-after-load 'sh-werk
+  (put 'shstruct 'backward-op-at
+       (lambda ()
+	 (re-search-backward ar-end-shstruct-atpt nil 'move 1))))
 
 ;; Symbol
 (put 'symbol 'beginning-op-at
@@ -2475,7 +2479,7 @@ Otherwise assume being behind an opening delimiter or at a closing "
 		  (point))))
 
 (put 'word 'end-op-at
-     (lambda () (when (and (or (bolp)(looking-back "\\W"))(looking-at "\\w"))
+     (lambda () (when (and (or (bolp)(looking-back "\\W" (line-end-position)))(looking-at "\\w"))
 		  (forward-word 1)(point))))
 
 ;; Word-alpha-only
