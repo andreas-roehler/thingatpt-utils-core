19aba1c45abfae33d1efdde2dc6a115106cdfed7
Andreas Roehler <andreas.roehler@online.de>
2023-01-24 13:19:29 +0100

    copyright updated
    Signed-off-by: Andreas Roehler <andreas.roehler@online.de>

--- thingatpt-utils-core.el
+++ thingatpt-utils-core.el
@@ -1,6 +1,6 @@
 ;;; thingatpt-utils-core.el --- th-at-point edit functions -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2010-2022 Andreas Röhler, unless
+;; Copyright (C) 2010-2023 Andreas Röhler, unless
 ;; indicated otherwise
 
 ;; Author: Andreas Röhler <andreas.roehler@easy-emacs.de>, unless
